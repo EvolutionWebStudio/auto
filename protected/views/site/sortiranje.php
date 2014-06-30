f69d2fa1c21120000dfa5f44acffb535e02b4a86
Ilja Tešić <ilija.tesic@gmail.com>
2014-06-30 20:02:15 +0200

    Doradjen dizajn sortiranja #35

--- protected/views/site/sortiranje.php
+++ protected/views/site/sortiranje.php
@@ -1,46 +1,62 @@
-<?php
-$str_js = "
-        var fixHelper = function(e, ui) {
-            ui.children().each(function() {
-                $(this).width($(this).width());
-            });
-            return ui;
-        };
- 
-        $('#project-grid table.items tbody').sortable({
-            forcePlaceholderSize: true,
-            forceHelperSize: true,
-            items: 'tr',
-            update : function () {
-                serial = $('#project-grid table.items tbody').sortable('serialize', {key: 'items[]', attribute: 'class'});
-                $.ajax({
-                    'url': '" . $this->createUrl('//car/sort') . "',
-                    'type': 'post',
-                    'data': serial,
-                    'success': function(data){
 
+
+<div class="page clearfix car-sorting">
+    <div class="large-12 columns">
+        <h1 class="page-title">Sortiranje vozila</h1>
+    </div>
+    <div class="large-12 columns">
+
+        <?php
+        $str_js = "
+                var fixHelper = function(e, ui) {
+                    ui.children().each(function() {
+                        $(this).width($(this).width());
+                    });
+                    return ui;
+                };
+
+                $('#project-grid table.items tbody').sortable({
+                    forcePlaceholderSize: true,
+                    forceHelperSize: true,
+                    opacity: 0.8,
+                    cursor: 'move',
+                    placeholder: 'ui-sortable-placeholder',
+                    revert: 200,
+                    items: 'tr',
+                    update : function () {
+                        serial = $('#project-grid table.items tbody').sortable('serialize', {key: 'items[]', attribute: 'class'});
+                        $.ajax({
+                            'url': '" . $this->createUrl('//car/sort') . "',
+                            'type': 'post',
+                            'data': serial,
+                            'success': function(data){
+
+                            },
+                            'error': function(request, status, error){
+                                alert('We are unable to set the sort order at this time.  Please try again in a few minutes.');
+                            }
+                        });
                     },
-                    'error': function(request, status, error){
-                        alert('We are unable to set the sort order at this time.  Please try again in a few minutes.');
-                    }
-                });
-            },
-            helper: fixHelper
-        }).disableSelection();
-    ";
+                    helper: fixHelper
+                }).disableSelection();
+            ";
 
-Yii::app()->clientScript->registerScript('sortable-project', $str_js);
-?>
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'project-grid',
-	'dataProvider'=>$model,
-	'rowCssClassExpression'=>'"items[]_{$data->id}"',
-	'ajaxUpdate'=>false,
-	'columns'=>array(
-		'naslov',
-		'sort_order',
-	),
-));
+        Yii::app()->clientScript->registerScript('sortable-project', $str_js);
 
-Yii::app()->clientScript->registerScriptFile('http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js');
-?>
+        $this->widget('zii.widgets.grid.CGridView', array(
+            'id'=>'project-grid',
+            'template' => '{items}',
+            'dataProvider'=>$model,
+            'rowCssClassExpression'=>'"items[]_{$data->id}"',
+            'ajaxUpdate'=>false,
+            'columns'=>array(
+                'naslov',
+            ),
+            'hideHeader' => true,
+        ));
+
+        Yii::app()->clientScript->registerScriptFile('http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js');
+        ?>
+
+    </div>
+</div>
